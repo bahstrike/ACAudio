3f62e27285c1751196b4e68c36c5437d5299a1a8
bahstrike <strike_1337@hotmail.com>
2022-11-28 19:13:57 -0500

    +add initial server project

---
+++ ACAudioVCServer/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ACAudioVCServer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logLB = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // logLB
+            // 
+            this.logLB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logLB.FormattingEnabled = true;
+            this.logLB.Location = new System.Drawing.Point(12, 12);
+            this.logLB.Name = "logLB";
+            this.logLB.Size = new System.Drawing.Size(651, 498);
+            this.logLB.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 513);
+            this.Controls.Add(this.logLB);
+            this.Name = "MainForm";
+            this.Text = "ACAudio VoiceChat Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox logLB;
+    }
+}
+
---
+++ ACAudioVCServer/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACAudioVCServer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void Log(string s)
+        {
+            while (logLB.Items.Count > 100)
+                logLB.Items.RemoveAt(0);
+
+            logLB.TopIndex = logLB.Items.Add(s);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Log("Startup");
+
+            
+        }
+    }
+}
